017655c54619d498ea93a90a646e7edcfa7cfe9f
warvick <viktor.varga.p60@gmail.com>
2026-04-23 17:24:54 +0200

    adding automated tests

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+blahblah
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name          string
+		inputHeader   string
+		expectedValue string
+	}{
+		{
+			name:          "Valid Header",
+			inputHeader:   "ApiKey 12345",
+			expectedValue: "12345",
+		},
+		{
+			name:          "Empty Header",
+			inputHeader:   "",
+			expectedValue: "",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			headers := http.Header{}
+			if tc.inputHeader != "" {
+				headers.Add("Authorization", tc.inputHeader)
+			}
+
+			got, _ := GetAPIKey(headers)
+			if got != tc.expectedValue {
+				t.Errorf("failed %s: expected %v, got %v", tc.name, tc.expectedValue, got)
+			}
+		})
+	}
+}
